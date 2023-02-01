67c03993ae25534ab9c84e36a487dcb792a58eaa
edenyzl2025 <edenyzl2025@student.cis.edu.hk>
2023-02-01 12:11:33 +0800

    my edits

--- src/AScannerDarkly.java
+++ src/AScannerDarkly.java
@@ -1,22 +1,35 @@
+import java.util.Scanner;
+
 public class AScannerDarkly {
 
     public static void main(String[] args) {
 
         // 1. Create a Scanner object called 'scanner' that reads user input.
 
+        Scanner scanner = new Scanner(System.in);
+
         // 2. Print out a message that says "Please enter your first name: "
 
+        System.out.println("Please enter your first name:");
+
         // 3. Store the input into a variable called 'firstName'.
+        String firstName = scanner.next();
 
         // 4. Print out a message that says "Please enter your last name: "
+        System.out.println("Please enter your last name:");
 
         // 5. Store the input into a variable called 'lastName'.
+        String lastName = scanner.next();
 
         // 6. Print out a message that says "Please enter your age: "
+        System.out.println("Please enter your age");
 
         // 7. Store the input into a variable called 'age'.
+        int age = scanner.nextInt();
+
 
         // 8. Store all the details of the user into a variable called 'userInfo'.
+
 
             /* The format for this String should be, for example, as follows:
 
@@ -28,8 +41,10 @@ public class AScannerDarkly {
 
         // 9. Print out the variable 'userInfo'.
 
+        String userInfo = "First name: "+firstName+"\nLast name: "+lastName+"\nAge: "+age;
+        System.out.println(userInfo);
         // 10. Close the Scanner object.
-
+        scanner.close();
     }
 
 }
